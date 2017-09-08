7b923cbaecd11d188551d59738e7c2ac920a059b
Sai Krishna <selenium.saikrishna@gmail.com>
2017-09-08 02:08:28 -0700

    First Commit

---
+++ src/Sample.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+
+public class Sample {
+
+	
+	public static void main(String[] args) {
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("http://localhost:8899/webapp");
+		String exptext="Hello, World!";
+		String acttext=driver.findElement(By.xpath("html/body")).getText();
+		System.out.println("Expected text : "+exptext);
+		System.out.println("Actual text : "+acttext);
+		
+		if(exptext.equals(acttext))
+			System.out.println("Passed");
+		else
+			System.out.println("Failed");
+		//Assert.assertEquals("5","6");
+		driver.close();
+		driver.quit();
+		}
+
+}
